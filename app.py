194e5a148e78d0d86b49063f2358cbc0f5c5d466
Hereugo <thereug6@gmail.com>
2021-03-08 10:17:20 +0600

    make it better

--- app.py
+++ app.py
@@ -432,4 +432,4 @@ def webhook():
 
 
 if __name__ == "__main__":
-	server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+	app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
